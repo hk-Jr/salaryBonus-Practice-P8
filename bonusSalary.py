4d95eaa6b3d1a212d026fef66019e9d528a28f5b
kishna h k <khk803123@gmail.com>
2025-10-23 15:10:09 +0530

    added tax deduction.

--- bonusSalary.py
+++ bonusSalary.py
@@ -2,6 +2,10 @@
 salary = float(input("Enter employee salary: "))
 print("Original Salary:", salary)
 
-
+#10%
 bonus_salary = salary + (salary * 0.10)
 print("Bonus Added:", bonus_salary)
+
+# 5%
+final_salary = bonus_salary - (bonus_salary * 0.05)
+print("After Tax Deduction:", final_salary)
